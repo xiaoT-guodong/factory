661a1933e720b6160d06abf2f6e6aac94f0cd2b0
xiaoT <13003672533@163.com>
2021-01-04 16:40:39 +0800

    增加跨域

---
+++ src/main/java/com/xiaot/factory/config/CrossConfig.java
@@ -0,0 +1,19 @@
+package com.xiaot.factory.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrossConfig implements WebMvcConfigurer {
+
+    private final String[] methods = new String[]{"GET", "POST", "PUT", "DELETE"};
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(methods);
+    }
+
+}
